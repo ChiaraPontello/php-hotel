23d9b14a8c6361905b51a9e86c7a476dff161f9d
ChiaraPontello <chiarapontello1@gmail.com>
2023-11-29 16:07:42 +0100

    table bootstrap

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
+<?php
+include __DIR__ .'/Model/hotels.php';
+ ?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,5 +14,47 @@
 </head>
 <body>
     
+    <header class="container">
+        <h1>
+            Hotel
+        </h1>
+    </header>
+
+    <main class="container">
+    <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td colspan="2">Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
+    </main>
+
+    <footer class="container">
+
+    </footer>
+
 </body>
 </html>
